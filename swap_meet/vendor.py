7591e28067b0380649168141ba0f443e6d10a8e3
mami <mamypoco@gmail.com>
2025-09-30 15:12:49 -0700

    updated main and pulled

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -5,31 +5,10 @@ class Vendor:
     def add(self, item):
         self.inventory.append(item)
         return item
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 46edd41e1121f744ce5895834e0728074cb55505
     def remove(self, item):
         if item not in self.inventory:
             return False
 
-<<<<<<< HEAD
-        # item = "Orange"
-        # ["apple", "orange", "banana"]
-        for index in range(len(self.inventory)):
-            fruit = self.inventory[index]
-            if fruit == item:
-                #["apple", "banana", "banana"]
-                #["apple", "banana"] 
-                self.inventory[index] = self.inventory[-1] 
-                self.inventory.pop()
-                # vs self.inventory.pop(index)
-
-        return item
-            
-        
-=======
         for index in range(len(self.inventory)):
             current_item = self.inventory[index]
             if current_item == item:
@@ -44,4 +23,3 @@ class Vendor:
             if item.id == id:        
                 return item
         return None
->>>>>>> 46edd41e1121f744ce5895834e0728074cb55505
