0a17e370221d44cbf60075839efdb3e7c2c21b16
mami <mamypoco@gmail.com>
2025-09-30 19:28:19 -0700

    wave03 and 04 passed tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from .item import Item
+
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
@@ -23,3 +25,37 @@ class Vendor:
             if item.id == id:        
                 return item
         return None
+
+
+    def swap_items(self, other_vendor, my_item, their_item):
+
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        
+        self.remove(my_item)
+        other_vendor.add(my_item)
+        
+        other_vendor.remove(their_item)
+        self.add(their_item)
+
+        return True
+    
+    def swap_first_item(self, other_vendor):
+
+        if len(self.inventory) == 0 or len(other_vendor.inventory) == 0:
+            return False
+        
+        my_first_item = self.inventory[0]
+        friend_first_item = other_vendor.inventory[0]
+        
+        this_item_removed = self.remove(my_first_item)
+        other_vendor.add(this_item_removed)
+        
+        friend_item_removed = other_vendor.remove(friend_first_item)
+        self.add(friend_item_removed)
+
+        return True
+
+
+
+
