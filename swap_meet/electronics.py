822e8a3dbdfacb9740ea92b56c3e795125193141
mami <mamypoco@gmail.com>
2025-10-01 13:25:23 -0700

    wave05 passed test

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,14 @@
-class Electronics:
-    pass
+from .item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None, type="Unknown", condition=0):
+        super().__init__(id, condition) # common attributes
+        self.type= type # only self (child)
+
+    def get_category(self):
+        return "Electronics"
+    
+    def __str__(self):
+        return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
+    
+    # Parent already have the condition_desription
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,31 @@
 import uuid
+from .constants import CONDITIONS
+
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         self.id = int(uuid.uuid4()) if id is None else id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
 
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+    
+    def condition_description(self):
+        return CONDITIONS.get(self.condition)       
+
+        # match self.condition:
+        #     case int(5):
+        #         msg = "Excellent"
+        #     case int(4):
+        #         msg = "Good"
+        #     case int(3):
+        #         msg = "Fair"
+        #     case int(2):
+        #         msg = "Well used"
+        #     case int(1):
+        #         msg = "Poor"
+        #     case int(0):
+        #         msg = "Take only for yourself"
+        # return msg
