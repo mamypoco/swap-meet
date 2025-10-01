d52297d5e0885efe4b5f8769c4e3ee83f567b1ed
Makiko <trimakichan@gmail.com>
2025-10-01 14:05:54 -0700

    complete wave 5 and pass all the tests.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,17 @@
-import uuid
+from uuid import uuid4
+from .constants import CONDITIONS
 class Item:
-    def __init__(self, id=None):
-        self.id = int(uuid.uuid4()) if id is None else id
+    def __init__(self, id=None, condition=0):
+        self.id = int(uuid4()) if id is None else id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
 
     def __str__(self):
         return f"An object of type Item with id {self.id}."
+
+    def condition_description(self):
+        msg = CONDITIONS[self.condition]
+        return msg
 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,10 +18,32 @@ class Vendor:
                 break
 
         return item
-    
+
     def get_by_id(self, id):
         for item in self.inventory:
-            if item.id == id:        
+            if item.id == id:
                 return item
         return None
 
+    def swap_items(self, other_vendor, my_item, their_item):
+
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+
+        self.remove(my_item)
+        other_vendor.add(my_item)
+
+        other_vendor.remove(their_item)
+        self.add(their_item)
+
+        return True
+
+    def swap_first_item(self, other_vendor):
+    
+        if not self.inventory or not other_vendor.inventory:
+            return False
+
+        my_first_item = self.inventory[0]
+        their_first_item = other_vendor.inventory[0]
+
+        return self.swap_items(other_vendor, my_first_item, their_first_item)
